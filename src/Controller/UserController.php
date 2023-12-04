f8b76646a7a3fd398240e11ec8b388ac82a83c6d
Gabriel Camurça <gabriel@brtech.dev>
2023-12-04 13:12:48 -0300

    Implements twig pages && controller

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,16 +2,17 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route("/", name: "web_user_")]
-class UserController
+class UserController extends AbstractController
 {
     #[Route("/", methods: ["GET"], name: "index")]
     public function index(): Response
     {
-        return new Response("Implements register form");
+        return $this->render("user/form.html.twig");
     }
 
 
