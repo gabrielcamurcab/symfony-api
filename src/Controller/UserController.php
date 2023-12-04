af8ab8faff542e5d691ae01ad1c92aaf21bf8955
Gabriel Camurça <gabriel@brtech.dev>
2023-12-04 16:11:17 -0300

    Implements user create on web controller

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,8 +20,23 @@ class UserController extends AbstractController
 
 
     #[Route("/save", methods: ["POST"], name: "save")]
-    public function save(): Response
+    public function save(Request $request, EntityManagerInterface $entityManager): Response
     {
+        $data = $request->request->all();
+
+        $user = new User;
+        $user->setName($data['name']);
+        $user->setEmail($data['email']);
+
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        if ($user->getId())
+            return $this->render("user/success.html.twig", ["user_name" => $user->getName()]);
+        else
+            return $this->render("user/error.html.twig", ["user_name" => $user->getName()]);
+        
+
         return new Response("Implements save function");
     }
 }
